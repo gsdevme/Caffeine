b1f7dc738207ec31152e975f95a0e5cb0a840055
Gavin Staniforth <github.efji3n6i@gsdev.me>
2015-01-15 22:57:37 +0000

    wip

--- src/Caffeine/Console/Command/ConfigCommand.php
+++ src/Caffeine/Console/Command/ConfigCommand.php
@@ -3,6 +3,7 @@
 namespace Caffeine\Console\Command;
 
 use Caffeine\Storage\Configuration;
+use Caffeine\Storage\StorageService;
 use DateTimeZone;
 use Symfony\Component\Console\Helper\HelperInterface;
 use Symfony\Component\Console\Helper\HelperSet;
@@ -18,10 +19,6 @@ use Symfony\Component\Console\Question\Question;
 class ConfigCommand extends Command
 {
     const ARGUMENT_TWITCH_CHANNEL = 'twitch-channel-name';
-    const ARGUMENT_OATUH_TOKEN    = 'bot-oauth-token';
-    const ARGUMENT_USERNAME       = 'username';
-    const ARGUMENT_TIMEZONE       = 'timezone';
-    const ARGUMENT_ADMIN_USERS    = 'admin-users';
 
     const QUESTION_OAUTH    = '<question>Please enter the OAuth token for the bot. (http://goo.gl/mBNASR): </question>';
     const QUESTION_USERNAME = '<question>Please enter the username for the bot: </question>';
@@ -61,13 +58,6 @@ class ConfigCommand extends Command
         $this->addArguments([
             [self::ARGUMENT_TWITCH_CHANNEL, 'Twitch Channel']
         ], InputArgument::REQUIRED);
-
-        $this->addArguments([
-            [self::ARGUMENT_OATUH_TOKEN, 'OAuth token for the bot, to override the configuration'],
-            [self::ARGUMENT_USERNAME, 'Bot username'],
-            [self::ARGUMENT_ADMIN_USERS, 'List of admin users which can add commands'],
-            [self::ARGUMENT_TIMEZONE, 'Timezone i.e. (Europe/London)'],
-        ], InputArgument::OPTIONAL);
     }
 
     /**
@@ -77,56 +67,51 @@ class ConfigCommand extends Command
     {
         $channel = $input->getArgument(self::ARGUMENT_TWITCH_CHANNEL);
 
-        $this->writeInfo($output, ' -Twitch Channel: ' . $channel);
-        $this->setProcessTitle('Caffeine-' . $channel);
-
         $this->configService($channel, $output);
 
-        $oauth    = $this->getOptionalOrPrompt($input, $output, self::ARGUMENT_OATUH_TOKEN, self::QUESTION_OAUTH);
-        $username = $this->getOptionalOrPrompt($input, $output, self::ARGUMENT_USERNAME, self::QUESTION_USERNAME);
-        $timezone = new DateTimeZone($this->getOptionalOrPrompt($input, $output, self::ARGUMENT_TIMEZONE, self::QUESTION_TIMEZONE, 'UTC'));
+        $storage = new StorageService([$this->configuration->getCurrentWorkingDirectory()]);
+        $data = $storage->load($this->configuration->getConfigurationFilePath($channel));
+
+        var_dump($data);
+        die;
+
+        $oauth    = $this->getOptionalOrPrompt($input, $output, self::QUESTION_OAUTH);
+        $username = $this->getOptionalOrPrompt($input, $output, self::QUESTION_USERNAME);
+        $timezone = new DateTimeZone($this->getOptionalOrPrompt($input, $output, self::QUESTION_TIMEZONE, 'UTC'));
+
+
 
         $this->writeInfo($output, ' -OAuth Token: ' . $oauth);
         $this->writeInfo($output, ' -Username: ' . $username);
         $this->writeInfo($output, ' -Timezone: ' . $timezone->getName());
-
-        //$storage = new Caffeine\Storage\StorageService([$configuration->getCurrentWorkingDirectory()]);
-        //$data = $storage->load($configuration->getConfigurationFilePath($channel));
     }
 
     /**
      * @param InputInterface $input
      * @param OutputInterface $output
-     * @param string $name
-     * @param string $question
-     * @param string $default
-     * @return mixed|string
+     * @param $question
+     * @param null $default
+     * @return string
      */
-    private function getOptionalOrPrompt(InputInterface $input, OutputInterface $output, $name, $question, $default = null)
+    private function getOptionalOrPrompt(InputInterface $input, OutputInterface $output, $question, $default = null)
     {
-        $argument = $input->getArgument($name);
+        /** @var \Symfony\Component\Console\Helper\QuestionHelper $helper */
+        $helper = $this->getHelper('question');
 
-        if ($argument === null) {
-            /** @var \Symfony\Component\Console\Helper\QuestionHelper $helper */
-            $helper = $this->getHelper('question');
+        $question = new Question($question, $default);
 
-            $question = new Question($question, $default);
+        /**
+         * MAJOR TODO HERE, SORT THIS SHIT OUT
+         */
+        $question->setValidator(function ($v) {
+            if (strlen($v) === 0) {
+                throw new \Exception('Value can not be empty.');
+            }
 
-            /**
-             * MAJOR TODO HERE, SORT THIS SHIT OUT
-             */
-            $question->setValidator(function ($v) {
-                if (strlen($v) === 0) {
-                    throw new \Exception('Value can not be empty.');
-                }
+            return $v;
+        });
 
-                return $v;
-            });
-
-            $argument = $helper->ask($input, $output, $question);
-        }
-
-        return $argument;
+        return $helper->ask($input, $output, $question);
     }
 
     /**
@@ -138,12 +123,12 @@ class ConfigCommand extends Command
         $config = $this->configuration->getConfigurationFilePath($channel);
 
         if (!$this->configuration->exists($channel)) {
-            $this->writeInfo($output, ' -Creating configuration file: ' . $config);
+            $this->writeInfo($output, 'Creating configuration file: ' . $config);
 
             $this->configuration->mkdir($this->configuration->getFolderStoragePath($channel), 0755);
             $this->configuration->touch($channel);
         } else {
-            $this->writeInfo($output, ' -Using configuration file: ' . $config);
+            $this->writeInfo($output, 'Using configuration file: ' . $config);
         }
     }
 }
